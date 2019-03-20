3ae0311137658e4a6a07ba06ec021bc8b00ce35f
phosphene47 <phosphene47@chiya.dev>
2019-03-20 20:59:56 +1100

    Add AutoRollGender

--- MudaeFarm/Config.cs
+++ MudaeFarm/Config.cs
@@ -6,6 +6,7 @@ namespace MudaeFarm
     {
         public string AuthToken { get; set; }
         public double? AutoRollInterval { get; set; }
+        public char AutoRollGender { get; set; } = 'w';
 
         public HashSet<ulong> BotChannels { get; set; } = new HashSet<ulong>();
 

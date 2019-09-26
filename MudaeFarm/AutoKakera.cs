1ed2a671e90dcacc858e3a874fe4957208c35ad3
phosphene47 <phosphene47@chiya.dev>
2019-09-26 15:45:36 +1000

    Fix message not being retrieved

--- MudaeFarm/AutoKakera.cs
+++ MudaeFarm/AutoKakera.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+
+// async methods with no await
+#pragma warning disable 1998
 
 namespace MudaeFarm
 {
@@ -31,7 +36,38 @@ namespace MudaeFarm
             { "kakeraW", KakeraType.Rainbow }
         };
 
-        public void Initialize() => _client.ReactionAdded += HandleReactionAsync;
+        public void Initialize()
+        {
+            _client.MessageReceived += HandleMessageAsync;
+            _client.ReactionAdded   += HandleReactionAsync;
+        }
+
+        async Task HandleMessageAsync(SocketMessage message)
+        {
+            if (!(message is SocketUserMessage userMessage))
+                return;
+
+            if (!MudaeInfo.IsMudae(message.Author))
+                return;
+
+            if (!message.Embeds.Any())
+                return;
+
+            // channel must be enabled for claiming
+            if (!_config.BotChannelIds.Contains(message.Channel.Id))
+                return;
+
+            // remember message
+            _messageCache[message.Id] = userMessage;
+
+            foreach (var m in _messageCache.Values)
+            {
+                if ((DateTimeOffset.Now - m.Timestamp).TotalMinutes >= 5)
+                    _messageCache.TryRemove(m.Id, out _);
+            }
+        }
+
+        readonly ConcurrentDictionary<ulong, IUserMessage> _messageCache = new ConcurrentDictionary<ulong, IUserMessage>();
 
         async Task HandleReactionAsync(Cacheable<IUserMessage, ulong> cacheable, ISocketMessageChannel channel, SocketReaction reaction)
         {
@@ -40,6 +76,10 @@ namespace MudaeFarm
 
             // channel must be enabled for claiming
             if (!_config.BotChannelIds.Contains(channel.Id))
+                return;
+
+            // retrieve message
+            if (!_messageCache.TryGetValue(reaction.MessageId, out var message))
                 return;
 
             // reactor must be mudae
@@ -66,22 +106,6 @@ namespace MudaeFarm
                 return;
 
             state.KakeraPower -= state.KakeraPowerConsumption;
-
-            // retrieve message
-            IUserMessage message;
-
-            try
-            {
-                message = await channel.GetMessageAsync(reaction.MessageId) as IUserMessage;
-
-                if (message == null)
-                    return;
-            }
-            catch (Exception e)
-            {
-                Log.Warning($"Could not get message {reaction.MessageId}.", e);
-                return;
-            }
 
             // claim kakera
             await Task.Delay(_config.KakeraClaimDelay);

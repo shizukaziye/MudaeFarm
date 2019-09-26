268041567ca9f3ede1b346feafadc4ef059f87d2
phosphene47 <phosphene47@chiya.dev>
2019-09-26 13:11:48 +1000

    Fix $tu parser

--- MudaeFarm/TimersUpParser.cs
+++ MudaeFarm/TimersUpParser.cs
@@ -21,7 +21,7 @@ namespace MudaeFarm
             int.TryParse(match.Groups["hour"].Value, out var hours);
             int.TryParse(match.Groups["minute"].Value, out var minutes);
 
-            time = new TimeSpan(0, hours, minutes);
+            time = new TimeSpan(hours, minutes, 0);
             return true;
         }
 
@@ -30,10 +30,15 @@ namespace MudaeFarm
         static bool TryParseInt(string str, out int value)
             => int.TryParse(_intRegex.Match(str).Value, out value);
 
-        public static bool TryParse(IDiscordClient client, IMessage message, MudaeState state)
+        public static bool TryParse(IDiscordClient client, IMessage message, out MudaeState state)
         {
             if (!message.Content.StartsWith($"**{client.CurrentUser.Username}**"))
+            {
+                state = null;
                 return false;
+            }
+
+            state = new MudaeState();
 
             var now    = DateTime.Now;
             var failed = 0;
@@ -44,37 +49,31 @@ namespace MudaeFarm
             {
                 var line = line1.ToLowerInvariant();
 
-                if (line.Contains("claim") && line.Contains("reset"))
+                if (line.Contains("claim"))
                 {
                     if (TryParseTime(line, out var time))
                         state.ClaimReset = now + time;
-                    else
-                        state.ClaimReset = null;
                 }
 
                 else if (line.Contains("rolls") && line.Contains("left"))
                 {
-                    TryParseInt(line, out var value);
-
-                    state.RollsLeft = value;
+                    if (TryParseInt(line, out var value))
+                        state.RollsLeft = value;
                 }
 
                 else if (line.Contains("rolls") && line.Contains("reset"))
                 {
                     if (TryParseTime(line, out var time))
                         state.RollsReset = now + time;
-                    else
-                        state.RollsReset = null;
                 }
 
-                else if (line.Contains("react") && line.Contains("kakera"))
+                else if (!line.Contains("power") && line.Contains("react") && line.Contains("kakera"))
                 {
                     if (TryParseTime(line, out var time))
                         state.KakeraReset = now + time;
+
                     else if (line.Contains("now"))
                         state.KakeraReset = now;
-                    else
-                        state.KakeraReset = null;
                 }
 
                 else if (line.Contains("power") && line.Contains("kakera"))
@@ -83,39 +82,33 @@ namespace MudaeFarm
 
                     foreach (var part in parts)
                     {
-                        if (part.Contains("power"))
-                        {
-                            if (TryParseInt(part, out var value))
-                                state.KakeraPower = value / 100.0;
-                            else
-                                state.KakeraPower = 0;
-                        }
-
-                        else if (part.Contains("consume"))
+                        if (part.Contains("consume"))
                         {
                             if (TryParseInt(part, out var value))
                                 state.KakeraPowerConsumption = value / 100.0;
-                            else
-                                state.KakeraPowerConsumption = 0;
+                        }
+
+                        else if (part.Contains("power"))
+                        {
+                            if (TryParseInt(part, out var value))
+                                state.KakeraPower = value / 100.0;
                         }
                     }
                 }
 
                 else if (line.Contains("stock") && line.Contains("kakera"))
                 {
-                    TryParseInt(line, out var value);
-
-                    state.KakeraStock = value;
+                    if (TryParseInt(line, out var value))
+                        state.KakeraStock = value;
                 }
 
                 else if (line.Contains("$dk"))
                 {
                     if (TryParseTime(line, out var time))
                         state.KakeraDailyReset = now + time;
+
                     else if (line.Contains("ready"))
                         state.KakeraDailyReset = now;
-                    else
-                        state.KakeraDailyReset = null;
                 }
 
                 else
@@ -124,8 +117,34 @@ namespace MudaeFarm
                 }
             }
 
+            if (state.ClaimReset == null)
+            {
+                Log.Warning("Could not parse claim reset time! Disabling indefinitely.");
+                state.ClaimReset = DateTime.MaxValue;
+            }
+
+            if (state.RollsReset == null)
+            {
+                Log.Warning("Could not parse roll reset time! Disabling indefinitely.");
+                state.RollsReset = DateTime.MaxValue;
+            }
+
+            if (state.KakeraReset == null)
+            {
+                Log.Warning("Could not parse kakera reset time! Disabling indefinitely.");
+                state.KakeraReset = DateTime.MaxValue;
+            }
+
+            if (state.KakeraDailyReset == null)
+            {
+                Log.Warning("Could not parse daily kakera reset time! Disabling indefinitely.");
+                state.KakeraDailyReset = DateTime.MaxValue;
+            }
+
             if (state.RollsLeft != 0 && state.RollsReset != null)
-                state.AverageRollInterval = new TimeSpan(state.RollsReset.Value.Ticks / state.RollsLeft);
+                state.AverageRollInterval = new TimeSpan((state.RollsReset.Value - now).Ticks / state.RollsLeft);
+            else
+                state.AverageRollInterval = TimeSpan.MaxValue;
 
             var success = failed < lines.Length / 2;
 

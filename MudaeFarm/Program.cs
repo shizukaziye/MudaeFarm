fbbf4aa24014d775de9dffc5fee1f74b9701eea4
phosphene47 <phosphene47@chiya.dev>
2019-09-28 02:10:15 +1000

    Implement auto-update

--- MudaeFarm/Program.cs
+++ MudaeFarm/Program.cs
@@ -1,12 +1,22 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MudaeFarm
 {
     public class Program
     {
-        static async Task Main()
+        static async Task Main(string[] args)
         {
+            if (!await HandleArgsAsync(args))
+            {
+                Console.ReadKey();
+                return;
+            }
+
             while (true)
             {
                 Console.Clear();
@@ -33,6 +43,65 @@ namespace MudaeFarm
 
                 await Task.Delay(TimeSpan.FromSeconds(10));
             }
+        }
+
+        static async Task<bool> HandleArgsAsync(IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                const string reinstallPrefix = "--reinstall=";
+
+                if (arg.StartsWith(reinstallPrefix))
+                {
+                    Log.Info($"Upgrading to v{UpdateChecker.CurrentVersion.ToString(3)}...");
+
+                    // wait for old process to exit
+                    await Task.Delay(TimeSpan.FromSeconds(3));
+
+                    var dir = Directory.CreateDirectory(arg.Substring(reinstallPrefix.Length));
+
+                    try
+                    {
+                        // delete all old files
+                        foreach (var file in dir.EnumerateFiles())
+                            file.Delete();
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+
+                    try
+                    {
+                        // copy ourselves in
+                        foreach (var file in new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).EnumerateFiles())
+                        {
+                            var dest = Path.Combine(dir.FullName, file.Name);
+
+                            file.CopyTo(dest, true);
+
+                            Log.Debug(dest);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error("Could not copy one of the files.", e);
+                        return false;
+                    }
+
+                    // run new installation
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName        = dir.EnumerateFiles("*.exe").First().FullName,
+                        UseShellExecute = false
+                    });
+
+                    Process.GetCurrentProcess().Kill();
+                    return true;
+                }
+            }
+
+            return true;
         }
     }
 }
--- MudaeFarm/UpdateChecker.cs
+++ MudaeFarm/UpdateChecker.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Octokit;
 
@@ -7,24 +11,54 @@ namespace MudaeFarm
 {
     public class UpdateChecker
     {
+        public static readonly Version CurrentVersion = typeof(Program).Assembly.GetName().Version;
+
         readonly GitHubClient _client = new GitHubClient(new ProductHeaderValue("MudaeFarm"));
 
         public async Task RunAsync()
         {
             try
             {
-                var currentVersion = typeof(Program).Assembly.GetName().Version;
-
-                Log.Warning($"MudaeFarm v{currentVersion.ToString(3)} by chiya.dev");
+                Log.Warning($"MudaeFarm v{CurrentVersion.ToString(3)} by chiya.dev");
 
                 var latestRelease = await _client.Repository.Release.GetLatest("chiyadev", "MudaeFarm");
                 var latestVersion = Version.Parse(latestRelease.Name.TrimStart('v'));
 
-                if (currentVersion.CompareTo(latestVersion) >= 0)
+                if (CurrentVersion.CompareTo(latestVersion) >= 0)
                     return;
 
-                // newer version available
-                Log.Warning($"Version v{latestVersion.ToString(3)} available: {latestRelease.HtmlUrl}");
+                var asset = latestRelease.Assets.FirstOrDefault(a => a.Name.Contains(".zip"));
+
+                if (asset != null)
+                {
+                    Log.Info($"Downloading v{latestVersion.ToString(3)}...");
+
+                    using (var memory = new MemoryStream())
+                    {
+                        // download release onto memory
+                        using (var http = new HttpClient())
+                        using (var stream = await http.GetStreamAsync(asset.BrowserDownloadUrl))
+                            await stream.CopyToAsync(memory);
+
+                        memory.Position = 0;
+
+                        // extract to a temp place
+                        var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+                        using (var archive = new ZipArchive(memory, ZipArchiveMode.Read, true))
+                            archive.ExtractToDirectory(path);
+
+                        // run new installation in update mode
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName        = new DirectoryInfo(path).EnumerateFiles("*.exe").First().FullName,
+                            Arguments       = $"--reinstall=\"{AppDomain.CurrentDomain.BaseDirectory}\"",
+                            UseShellExecute = false
+                        });
+
+                        Process.GetCurrentProcess().Kill();
+                    }
+                }
 
                 try
                 {
@@ -35,9 +69,9 @@ namespace MudaeFarm
                     // ignored
                 }
             }
-            catch
+            catch (Exception e)
             {
-                Log.Info("Could not check for updates.");
+                Log.Debug("Could not check for updates.", e);
             }
         }
     }

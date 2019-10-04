4405d49dbf574fbb4eee7048f7285d4833e6a96e
phosphene47 <phosphene47@chiya.dev>
2019-10-04 22:37:17 +1000

    Fix ZipFileExtensions.ExtractToDirectory missing method

--- MudaeFarm/UpdateChecker.cs
+++ MudaeFarm/UpdateChecker.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Octokit;
+using FileMode = System.IO.FileMode;
 
 namespace MudaeFarm
 {
@@ -73,7 +74,7 @@ namespace MudaeFarm
                     var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
 
                     using (var archive = new ZipArchive(memory, ZipArchiveMode.Read, true))
-                        archive.ExtractToDirectory(path);
+                        await ExtractToDirectoryAsync(archive, path);
 
                     // run new installation in update mode
                     var current = Process.GetCurrentProcess();
@@ -91,6 +92,29 @@ namespace MudaeFarm
             catch (Exception e)
             {
                 Log.Debug("Could not install updates.", e);
+            }
+        }
+
+        static async Task ExtractToDirectoryAsync(ZipArchive archive, string path)
+        {
+            // we cannot use ZipFileExtensions.ExtractToDirectory
+            // https://github.com/dotnet/corefx/issues/26996
+            foreach (var entry in archive.Entries)
+            {
+                var filePath = Path.Combine(path, entry.FullName);
+
+                if (entry.FullName.EndsWith("/") && string.IsNullOrEmpty(entry.Name))
+                {
+                    // https://stackoverflow.com/questions/40223451/how-to-tell-if-a-ziparchiveentry-is-directory
+                    Directory.CreateDirectory(filePath);
+                    continue;
+                }
+
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                using (var entryStream = entry.Open())
+                using (var stream = File.Open(filePath, FileMode.Create, FileAccess.Write))
+                    await entryStream.CopyToAsync(stream);
             }
         }
     }

11fb9200331b5dbcbff8857244d675f7d7eed8b7
phosphene47 <phosphene47@chiya.dev>
2020-05-26 20:35:43 +1000

    Create MudaeFarm project

---
+++ MudaeFarm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MudaeFarm
+{
+    public static class Program
+    {
+        public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                   .ConfigureLogging(l =>
+                    {
+                        if (args.Contains("--verbose"))
+                            l.SetMinimumLevel(LogLevel.Trace);
+
+                        l.AddFile("log_{Date}.txt");
+                    })
+                   .ConfigureServices((builder, services) =>
+                    {
+                        /*services.AddHostedService<Worker>()*/
+                    });
+    }
+}

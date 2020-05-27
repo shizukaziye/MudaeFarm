5617d32e359c32b925ed843d309a9fb89f783f5d
phosphene47 <phosphene47@chiya.dev>
2020-05-27 22:34:43 +1000

    Add cooldown parser

--- MudaeFarm/MudaeOutputParser.cs
+++ MudaeFarm/MudaeOutputParser.cs
@@ -6,11 +6,14 @@ namespace MudaeFarm
     public interface IMudaeOutputParser
     {
         bool TryParseTime(string s, out TimeSpan time);
+        bool TryParseRollLimited(string s, out TimeSpan resetTime);
     }
 
     public class EnglishMudaeOutputParser : IMudaeOutputParser
     {
-        readonly Regex _timeRegex = new Regex(@"((?<hour>\d+)h\s*)?(?<minute>\d+)(\**)?\s*min", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase);
+        const RegexOptions _regexOptions = RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase;
+
+        static readonly Regex _timeRegex = new Regex(@"((?<hour>\d+)h\s*)?(?<minute>\d+)(\**)?\s*min", _regexOptions);
 
         public bool TryParseTime(string s, out TimeSpan time)
         {
@@ -21,12 +24,16 @@ namespace MudaeFarm
                 // hour is optional
                 int.TryParse(match.Groups["hour"].Value, out var hours);
 
-                time = new TimeSpan(0, hours, minutes);
+                time = new TimeSpan(hours, minutes, 0);
                 return true;
             }
 
             time = default;
             return false;
         }
+
+        static readonly Regex _rollLimitedRegex = new Regex(@"roulette\s+is\s+limited", _regexOptions);
+
+        public bool TryParseRollLimited(string s, out TimeSpan resetTime) => _rollLimitedRegex.IsMatch(s) & TryParseTime(s, out resetTime);
     }
 }
